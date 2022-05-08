7de16ee455d7ad60888981c485f362377eb315b6
houdini-21 <marinerocruzfer@gmail.com>
2022-05-07 21:16:17 -0600

    ya se pueden crear citas

--- src/app/pages/mantenimientos/razas/razas.component.ts
+++ src/app/pages/mantenimientos/razas/razas.component.ts
@@ -7,72 +7,77 @@ import { RazaService } from '../../../services/raza.service';
 @Component({
   selector: 'app-razas',
   templateUrl: './razas.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-
 export class RazasComponent implements OnInit {
-
   // tslint:disable-next-line: no-inferrable-types
   public cargando: boolean = true;
 
   public razas: Raza[] = [];
 
-  constructor(private razaService: RazaService,
-              private busquedasService: BusquedasService) { }
+  constructor(
+    private razaService: RazaService,
+    private busquedasService: BusquedasService
+  ) {}
 
   ngOnInit(): void {
-      this.cargarRazas();
+    this.cargarRazas();
   }
 
-  buscar( termino: string ) {
-
-    if ( termino.length === 0 ) {
+  buscar(termino: string) {
+    if (termino.length === 0) {
       return this.cargarRazas();
     }
 
-    this.busquedasService.buscar( 'razas', termino )
-        .subscribe( (resp: Raza[] ) => {
-          this.razas = resp;
-
-        });
+    this.busquedasService.buscar('razas', termino).subscribe((resp: Raza[]) => {
+      this.razas = resp;
+    });
   }
-  cargarRazas(){
+  cargarRazas() {
+    this.cargando = true;
 
-      this.cargando = true;
-
-      this.razaService.cargarRazas()
-          .subscribe(razas => {
-            this.cargando = false;
-            this.razas = razas;
-
-          });
+    this.razaService.cargarRazas().subscribe((razas) => {
+      this.cargando = false;
+      this.razas = razas;
+    });
   }
 
-  guardarCambios(raza: Raza): void{
-    this.razaService.actualizarRaza(raza._id, raza.nombre, raza.descripcion)
-        .subscribe(resp => {
-            Swal.fire('Actualizado', raza.nombre, 'success');
-        });
+  guardarCambios(raza: Raza): void {
+    this.razaService
+      .actualizarRaza(raza._id, raza.nombre, raza.descripcion)
+      .subscribe((resp) => {
+        Swal.fire('Actualizado', raza.nombre, 'success');
+      });
   }
 
-  eliminarRaza(raza: Raza){
-    this.razaService.borrarRaza(raza._id)
-        .subscribe(resp => {
-            this.cargarRazas();
-            Swal.fire('Eliminado', raza.nombre, 'success');
-        });
-
+  eliminarRaza(raza: Raza) {
+    this.razaService.borrarRaza(raza._id).subscribe((resp) => {
+      this.cargarRazas();
+      Swal.fire('Eliminado', raza.nombre, 'success');
+    });
   }
 
-  async abrirSweetAlert(){
-    const {value} = await Swal.fire<string>({
-          title: 'Agregar Especie',
-          text: 'Inrese la raza',
-          input: 'text',
-          inputPlaceholder: 'Nombre de la raza',
-          showCancelButton: true,
-        });
-      }
+  async abrirSweetAlert() {
+    //create a form with 2 inputs nombre and descripcion using sweetalert2
+    const { value: nombre } = await Swal.fire({
+      title: 'Nueva raza',
+      html: `
+            <input id="nombre" class="swal2-input" placeholder="Nombre">
+            <input id="descripcion" class="swal2-input" placeholder="Descripción">
+            `,
+      focusConfirm: false,
+      preConfirm: () => {
+        return [
+          (<HTMLInputElement>document.getElementById('nombre')).value,
+          (<HTMLInputElement>document.getElementById('descripcion')).value,
+        ];
+      },
+    });
+    if (nombre) {
+      this.razaService.crearRaza(nombre[0], nombre[1]).subscribe((resp) => {
+        this.cargarRazas();
+        Swal.fire('Creado', nombre[0], 'success');
+      });
+    }
+  }
 }
-
--- src/app/services/mascotas.service.ts
+++ src/app/services/mascotas.service.ts
@@ -7,13 +7,13 @@ import { catchError, map, tap } from 'rxjs/operators';
 
 import { environment } from 'src/environments/environment';
 
-import { Cita } from '../../models/cita.model';
+import { Mascota } from '../../models/mascotas.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class CitasService {
-  public citas: Cita;
+export class MascotasService {
+  public mascotas: Mascota[] = []; 
 
   constructor(private http: HttpClient) {}
 
@@ -29,31 +29,14 @@ export class CitasService {
     };
   }
 
-  cargarCitas() {
-    const url = `${environment.base_url}/citas`;
+  cargarMascotas() {
+    const url = `${environment.base_url}/mascotas`;
     return this.http.get(url, this.headers).pipe(
-      map((resp: { ok: boolean; citas: Cita[] }) => resp.citas),
+      map((resp: { ok: boolean; mascotas: Mascota[] }) => resp.mascotas),
       catchError((err) => {
         console.log(err);
         return of([]);
       })
     );
   }
-
-  actualizarCita(cita: Cita) {
-    let { observaciones, peso, tratamiento } = cita;
-    const url = `${environment.base_url}/citas/${cita._id}`;
-    return this.http.put(
-      url,
-      { observaciones, peso, tratamiento },
-      this.headers
-    );
-  }
-
-  crearCita(cita: Cita) {
-    const url = `${environment.base_url}/citas`;
-    return this.http.post(url, cita, this.headers);
-  }
-
-  buscar(termino: string) {}
 }

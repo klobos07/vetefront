0258051bf9640dc867484365879d29c07d686e49
Karen Lobos <klobos07@gmail.com>
2022-05-08 09:32:58 -0600

    guardar

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder,Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UsuarioService } from 'src/app/services/usuario.service';
 import Swal from 'sweetalert2';
 
-declare const gapi:any;
+declare const gapi: any;
 
 @Component({
   selector: 'app-login',
@@ -17,16 +17,16 @@ export class LoginComponent implements OnInit{
   public auth2: any;
 
   public loginForm = this.fb.group({
-    email:[localStorage.getItem('email') || '',[Validators.required, Validators.email]],
-    password: ['',Validators.required],
+    email: [localStorage.getItem('email') || '', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
     remember: [false]
   });
 
 
 
   constructor( private router: Router,
-                private fb: FormBuilder,
-                private usuarioService: UsuarioService ) { }
+               private fb: FormBuilder,
+               private usuarioService: UsuarioService ) { }
 
     ngOnInit(): void {
        this.renderButton();
@@ -38,24 +38,24 @@ export class LoginComponent implements OnInit{
         .subscribe(resp => {
 
           if (this.loginForm.get('remember').value){
-            localStorage.setItem('email',this.loginForm.get('email').value);
+            localStorage.setItem('email', this.loginForm.get('email').value);
           }else{
              localStorage.removeItem('email');
           }
           this.router.navigateByUrl('/');
-        }, (err) =>{
-          Swal.fire('Error',err.error.msg, 'error');
+        }, (err) => {
+          Swal.fire('Error', err.error.msg, 'error');
         });
-    //this.router.navigateByUrl('/');
+    // this.router.navigateByUrl('/');
   }
 
   renderButton() {
     gapi.signin2.render('my-signin2', {
-      'scope': 'profile email',
-      'width': 240,
-      'height': 50,
-      'longtitle': true,
-      'theme': 'dark',
+      scope: 'profile email',
+      width: 240,
+      height: 50,
+      longtitle: true,
+      theme: 'dark',
     });
 
     this.startApp();
@@ -67,9 +67,9 @@ export class LoginComponent implements OnInit{
           client_id: '902479618008-78keke18qou0n3thrjjqvgurcp97a15i.apps.googleusercontent.com',
           cookiepolicy: 'single_host_origin',
         });
-          this.attachSignin(document.getElementById('my-signin2'));
+        this.attachSignin(document.getElementById('my-signin2'));
       });
-    };
+    }
 
     attachSignin(element) {
 
@@ -78,7 +78,7 @@ export class LoginComponent implements OnInit{
             const id_token = googleUser.getAuthResponse().id_token;
             this.usuarioService.loginGoogle(id_token)
             .subscribe(resp => {
-              //Navegar al Dashboard
+              // Navegar al Dashboard
                this.router.navigateByUrl('/');
             });
 
